a579d6c4796afe0eb8e86e237c7d3b0126d21e73
Alessandra Lino <ale.lino.00@gmail.com>
2021-05-08 13:30:26 -0300

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>meu primeiro app!</h1>
+    </div>
+  );
+}
+
+export default App;
